fdd5086a8ad2b0b52ea278fad406bb70c552a752
Fabbernat <fabbernat@gmail.com>
2024-12-01 13:21:12 +0100

    3 func work, 3 TODO

---
+++ NAVEmailApp/utils/function3/NotificationService.cs
@@ -0,0 +1,19 @@
+﻿namespace NAVEmailApp.utils.function3;
+
+using System;
+using System.Collections.Generic;
+
+public class NotificationService
+{
+    public IEnumerable<string> GetInspectionNotifications()
+    {
+        // Mock data, could be fetched from a database or file in a real application.
+        return new List<string>
+        {
+            "Tóth Béla: Ellenőrzés szükséges 2024. október 31-én.",
+            "Kovács János: Ellenőrzés szükséges 2024. november 5-én.",
+            "Szabó Anna: Ellenőrzés szükséges 2024. december 19-én.",
+            "Nagy Péter: Ellenőrzés szükséges 2024. november 29-én."
+        };
+    }
+}
---
+++ NAVEmailApp/utils/function3/NotificationView.cs
@@ -0,0 +1,22 @@
+﻿namespace NAVEmailApp.utils.function3;
+
+using System;
+
+public class NotificationView
+{
+    private readonly NotificationService _notificationService;
+
+    public NotificationView(NotificationService notificationService)
+    {
+        _notificationService = notificationService;
+    }
+
+    public void DisplayNotifications()
+    {
+        Console.WriteLine("Ellenőrzési értesítések:");
+        foreach (var notification in _notificationService.GetInspectionNotifications())
+        {
+            Console.WriteLine($"- {notification}");
+        }
+    }
+}
